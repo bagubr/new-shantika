c1e2b7215479083d469b1e47a91ce1047214612f
Satrio Jati <satriotol69@gmail.com>
2021-07-07 12:33:33 +0700

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '089620755330',
+            'password' => Hash::make('admin')
+        ]);
     }
 }
