c385cc9e9b959ff4812e939c7e80de4e9a14c1e1
Satrio Jati <satriotol69@gmail.com>
2021-07-08 21:47:02 +0700

    create request

--- app/Http/Requests/CreateFleetClassRequest.php
+++ app/Http/Requests/CreateFleetClassRequest.php
@@ -24,7 +24,7 @@ class CreateFleetClassRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
+            'name' => 'required|unique:fleet_classes,name'
         ];
     }
 }
