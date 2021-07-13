db58ebb7d01cd713ad79952ed9999fd737ed9e6b
misft <indrawandwiprasetyo@gmail.com>
2021-07-13 10:58:42 +0700

    [update]

--- app/Models/Checkpoint.php
+++ app/Models/Checkpoint.php
@@ -14,4 +14,12 @@ class Checkpoint extends Model
     protected $fillable = [
         'route_id', 'arrived_at', 'agency_id', 'order'
     ];
+
+    public function route() {
+        return $this->belongsTo(Route::class, 'route_id', 'id');
+    }
+
+    public function agency() {
+        return $this->belongsTo(Agency::class, 'agency_id', 'id');
+    }
 }
