6bffa756525d445475d6d4f4b50a475dbb041349
Satrio Jati <satriotol69@gmail.com>
2021-07-13 19:27:53 +0700

    delete route done

--- app/Http/Controllers/RoutesController.php
+++ app/Http/Controllers/RoutesController.php
@@ -91,8 +91,10 @@ class RoutesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Route $route)
     {
-        //
+        $route->delete();
+        session()->flash('success', 'Route Deleted Successfully');
+        return redirect(route('routes.index'));
     }
 }
