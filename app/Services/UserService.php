bd3c790c8584f99b35ab5e2902d82063f5a1cfa9
misft <indrawandwiprasetyo@gmail.com>
2021-07-15 13:58:33 +0700

    [add] get profile and update profile customer

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,12 +5,22 @@ namespace App\Services;
 use App\Http\Requests\Api\ApiRegisterCustomerRequest;
 use App\Models\User;
 use App\Utils\Image;
+use Illuminate\Http\UploadedFile;
 
 class UserService {
-    public function register(array $data) {
+    public static function register(array $data) {
         Image::uploadFile($data['avatar'], 'avatar');
         $user = User::create($data);
         $user = AuthService::login($user, null, null, $data['uid']);
+        return $user;
+    }
+
+    public static function updateCustomerProfile(User $user, array $data) {
+        if(@$data['avatar'] instanceof UploadedFile) {
+            Image::uploadFile($data['avatar'], 'avatar');
+        }
+        $user->update($data);
+        $user->refresh();
         return $user;
     }
 }
--- routes/v1.php
+++ routes/v1.php
@@ -8,7 +8,6 @@ Route::group([
     Route::get('privacy_policy', 'PrivacyPolicyController@index');
     Route::get('informations', 'InformationController@index');
     Route::get('social_media', 'SocialMediaController@index');
-    Route::get('customer_menu', 'CustomerMenuController@index');
     Route::get('term_and_condition', 'TermAndConditionController@index');
     Route::get('faq', 'FaqController@index');
     Route::get('chat', 'ChatController@index');
@@ -43,10 +42,13 @@ Route::group([
         Route::post('login/uid', 'AuthController@loginUid');
         Route::post('registrasi','AuthController@registerCustomer');
 
+        Route::get('profile', 'UserController@showCustomer');
+        Route::post('update', 'UserController@updateProfileCustomer');
+
         Route::group([
             'middleware'=>'api.auth.user',
         ], function() {
-            //
+            Route::get('customer_menu', 'CustomerMenuController@index');
         });
     });
 });
