9db11ccbcb5190e14cb428c0d13e56486c58c73a
misft <indrawandwiprasetyo@gmail.com>
2021-07-16 07:00:43 +0700

    [refactor] refactor a lot of stuff

---
+++ routes/Factory/Api/v1/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+], function() {
+    Route::get('privacy_policy', 'PrivacyPolicyController@index');
+    Route::get('informations', 'InformationController@index');
+    Route::get('social_media', 'SocialMediaController@index');
+    Route::get('term_and_condition', 'TermAndConditionController@index');
+    Route::get('faq', 'FaqController@index');
+    Route::get('chat', 'ChatController@index');
+    Route::get('facility', 'FacilityController@index');
+    Route::get('cities', 'CityController@index');
+    Route::get('agencies', 'AgencyController@index');
+
+    Route::group(base_path('routes/Factory/Api/v1/Agen/index.php'));
+    Route::group(base_path('routes/Factory/Api/v1/Customer/index.php'));
+});
