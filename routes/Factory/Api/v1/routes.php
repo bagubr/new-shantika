a24d374fc54074531d8b0eb4f158ef8db7117b43
Bagu <bagubragowo3@gmail.com>
2021-07-16 14:51:41 +0700

    fix

--- routes/Factory/Api/v1/routes.php
+++ routes/Factory/Api/v1/routes.php
@@ -12,9 +12,16 @@ Route::group([
     Route::get('term_and_condition', 'TermAndConditionController@index');
     Route::get('faq', 'FaqController@index');
     Route::get('chat', 'ChatController@index');
+    
     Route::get('facility', 'FacilityController@index');
     Route::get('cities', 'CityController@index');
     Route::get('agencies', 'AgencyController@index');
+    Route::get('layouts', 'LayoutController@index');
+    Route::get('fleet_classes', 'FleetClassController@index');
+
+    Route::get('slider_detail/{id}', 'SliderController@sliderDetail');
+    Route::get('article_detail/{id}', 'ArticleController@articleDetail');
+    Route::get('testimonial_detail/{id}', 'TestimonialController@testimonialDetail');
 
     Route::group([],base_path('routes/Factory/Api/v1/Agen/index.php'));
     Route::group([],base_path('routes/Factory/Api/v1/Customer/index.php'));
