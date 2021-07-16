2ca3d8af0ab3350c7fc0b119c859e667a065ac94
misft <indrawandwiprasetyo@gmail.com>
2021-07-16 16:15:24 +0700

    [fix]

---
+++ app/Http/Controllers/v1/Agent/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\v1\Agent;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Repositories\UserRepository;
+use App\Services\ReviewService;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(Request $request) {
+        $user = UserService::getAuthenticatedUser($request->bearerToken());
+        $review = ReviewService::sumByUserIdForAgent($user->id);
+
+        return $this->sendSuccessResponse([
+            'user'=>$user,
+            'review'=>$review
+        ]);
+    }
+
+    public function update(Request $request) {
+        $user = UserService::getAuthenticatedUser($request->bearerToken());
+        $user = UserService::updateProfile($user, $request->toArray());
+
+        return $this->sendSuccessResponse([
+            'user'=>$user
+        ]);
+    }
+}
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,10 +4,14 @@ namespace App\Services;
 
 use App\Http\Requests\Api\ApiRegisterCustomerRequest;
 use App\Models\User;
+use App\Repositories\UserRepository;
 use App\Utils\Image;
+use App\Utils\Response;
 use Illuminate\Http\UploadedFile;
 
 class UserService {
+    use Response;
+
     public static function register(array $data) {
         Image::uploadFile($data['avatar'], 'avatar');
         $user = User::create($data);
@@ -21,6 +25,22 @@ class UserService {
         }
         $user->update($data);
         $user->refresh();
+        return $user;
+    }
+
+    public static function getAuthenticatedUser($token) {
+        $user = UserRepository::findByToken($token);
+
+        return $user 
+            ? $user 
+            : (new self)->sendFailedResponse([], 'Oops, sepertinya anda harus login ulang');
+    }
+
+    public static function updateProfile(User $user, array $data) {
+        Image::uploadFile($data['avatar'], 'avatar');
+        $user->update($data);
+        $user->refresh();
+
         return $user;
     }
 }
