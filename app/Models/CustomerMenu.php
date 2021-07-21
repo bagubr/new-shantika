16aa662701a282c16a608005894fa58bcfffae5f
Bagu <bagubragowo3@gmail.com>
2021-07-21 14:27:52 +0700

    fix customer menu order

--- app/Models/CustomerMenu.php
+++ app/Models/CustomerMenu.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Builder;
 
 class CustomerMenu extends Model
 {
@@ -13,7 +14,7 @@ class CustomerMenu extends Model
     protected $table = "customer_menus";
     protected $fillable = ['name', 'icon'];
 
-    protected static function booted()
+    public static function booted()
     {
         static::addGlobalScope('order', function (Builder $builder) {
             $builder->orderBy('id');
