ae069ba78ff39fe47e1d56627255e9d6ccbd6288
Satrio Jati <satriotol69@gmail.com>
2021-07-26 21:03:20 +0700

    merapikan order

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -27,105 +27,102 @@ Pemesanan
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Table Pemesanan</h3>
-                        <div class="text-right">
+                        {{-- <div class="text-right">
                             <a href="{{route('order.create')}}" class="btn btn-primary btn-sm">Tambah</a>
-                        </div>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Kode Order</th>
-                                    <th>Customer</th>
-                                    <th>Rute</th>
-                                    <th>Status</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($orders as $order)
-                                <tr>
-                                    <td>{{$order->code_order}}</td>
-                                    <td>{{$order->user->name}}</td>
-                                    <td>{{$order->route->name}}</td>
-                                    <td>{{$order->status}}</td>
-                                    <td>
-                                        <a href="{{route('order.edit',$order->id)}}"
-                                            class="btn btn-warning btn-xs">Edit</a>
-                                        <form action="{{route('order.destroy',$order->id)}}" class="d-inline"
-                                            method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-danger btn-xs"
-                                                onclick="return confirm('Are you sure?')" type="submit">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
+                    </div> --}}
                 </div>
-                <!-- /.card -->
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Kode Order</th>
+                                <th>Customer</th>
+                                <th>Rute</th>
+                                <th>Status</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($orders as $order)
+                            <tr>
+                                <td>{{$order->code_order}}</td>
+                                <td>{{$order->user->name}}</td>
+                                <td>{{$order->route->name}}</td>
+                                <td>{{$order->status}}</td>
+                                <td>
+                                    <form action="{{route('order.destroy',$order->id)}}" class="d-inline" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger btn-xs" onclick="return confirm('Are you sure?')"
+                                            type="submit">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
             </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Cari Pemesan</h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="{{route('order.search')}}" method="GET">
-                            <div class="form-group">
-                                <label>Cari Rute</label>
-                                <select name="route_id" id="" class="form-control select2">
-                                    <option value="">--Semua Rute--</option>
-                                    @foreach ($routes as $route)
-                                    @if (old('route_id') == $route->id)
-                                    <option value="{{$route->id}}" selected>{{$route->name}}</option>
-                                    @else
-                                    <option value="{{$route->id}}">{{$route->name}}</option>
-                                    @endif
-                                    @endforeach
-                                </select>
+            <!-- /.card -->
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Cari Pemesan</h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('order.search')}}" method="GET">
+                        <div class="form-group">
+                            <label>Cari Rute</label>
+                            <select name="route_id" id="" class="form-control select2">
+                                <option value="">--Semua Rute--</option>
+                                @foreach ($routes as $route)
+                                @if (old('route_id') == $route->id)
+                                <option value="{{$route->id}}" selected>{{$route->name}}</option>
+                                @else
+                                <option value="{{$route->id}}">{{$route->name}}</option>
+                                @endif
+                                @endforeach
+                            </select>
 
-                            </div>
-                            <div class="form-group">
-                                <label for="">Cari Status</label>
-                                <select name="status" class="form-control" id="">
-                                    <option value="">--Semua Status--</option>
-                                    @foreach ($status as $s)
-                                    @if (old('status') == $s)
-                                    <option value="{{$s}}" selected>{{$s}}</option>
-                                    @else
-                                    <option value="{{$s}}">{{$s}}</option>
-                                    @endif
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Agent</label>
-                                <select name="agent" id="" class="form-control">
-                                    <option value="">--Semua Pemesan--</option>
-                                    @foreach ($agent as $a)
-                                    @if (old('agent'))
-                                    <option value="{{$a}}" selected>{{$a}}</option>
-                                    @else
-                                    <option value="{{$a}}">{{$a}}</option>
-                                    @endif
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="text-right">
-                                <button class="btn btn-success" type="submit">Cari</button>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Cari Status</label>
+                            <select name="status" class="form-control" id="">
+                                <option value="">--Semua Status--</option>
+                                @foreach ($status as $s)
+                                @if (old('status') == $s)
+                                <option value="{{$s}}" selected>{{$s}}</option>
+                                @else
+                                <option value="{{$s}}">{{$s}}</option>
+                                @endif
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Agent</label>
+                            <select name="agent" id="" class="form-control">
+                                <option value="">--Semua Pemesan--</option>
+                                @foreach ($agent as $a)
+                                @if (old('agent'))
+                                <option value="{{$a}}" selected>{{$a}}</option>
+                                @else
+                                <option value="{{$a}}">{{$a}}</option>
+                                @endif
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="text-right">
+                            <button class="btn btn-success" type="submit">Cari</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
 @push('script')
