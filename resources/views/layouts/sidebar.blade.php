b3e3c6d765c31bc4aaba10b2f34542146f0de64b
Satrio Jati <satriotol69@gmail.com>
2021-07-27 14:52:47 +0700

    order details and order payment detail

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -17,14 +17,25 @@
         </div>
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
-                    <a href="{{route('dashboard')}}" class="nav-link {{Request::routeIs('dashboard') ? 'active' : ''}}">
+            <ul class="nav nav-pills nav-sidebar flex-column " data-widget="treeview" role="menu"
+                data-accordion="false">
+                <li class="nav-item {{Request::routeIs('dashboard') ? 'menu-open' : ''}}">
+                    <a href="#" class="nav-link {{Request::routeIs('dashboard') ? 'active' : ''}}">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
-                            Dasboard
+                            Dashboard
+                            <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{route('dashboard')}}"
+                                class="nav-link {{Request::routeIs('dashboard') ? 'active' : ''}}">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Dashboard Jawa</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
                 <li class="nav-item {{Request::routeIs('fleets.*','fleetclass.*','layouts.*') ? 'menu-open' : ''}}">
                     <a href="#"
