a8297632b3de001e2c3753551228de76a7a51b8e
Bagu <bagubragowo3@gmail.com>
2021-07-28 11:31:37 +0700

    fix tidak login di home

--- app/Http/Controllers/v1/Customer/HomeController.php
+++ app/Http/Controllers/v1/Customer/HomeController.php
@@ -19,9 +19,9 @@ class HomeController extends Controller
         $data['artikel'] = ArticleRepository::getAll();
         $data['testimonial'] = TestimonialRepository::getAll();
         $data['customer_menu'] = CustomerMenuRepository::getAll();
-
-        UserService::updateFcmToken(UserRepository::findByToken($request->bearerToken()), $request->token);
-
+        if(!empty($request->token)){
+            UserService::updateFcmToken(UserRepository::findByToken($request->bearerToken()), $request->token);
+        }
         $this->sendSuccessResponse($data);
     }
 }
