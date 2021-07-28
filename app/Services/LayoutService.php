8a9ecffc82c327bad65a69ad290ad5d8e86e96b0
misft <indrawandwiprasetyo@gmail.com>
2021-07-28 11:25:55 +0700

    [fix] unavailable

--- app/Services/LayoutService.php
+++ app/Services/LayoutService.php
@@ -15,10 +15,12 @@ class LayoutService {
         $layout->chairs = $layout->chairs->map(function ($item) use ($route, $date) {
             $item->is_booking = Booking::where('route_id', $route->id)->where('layout_chair_id', $item->id)->where('expired_at', '>=', date('Y-m-d H:i:s'))->exists();
             $item->is_unavailable = Order::where(function($query) use ($date) {
-                    $query->where('status', Order::STATUS1)
-                        ->where('status', Order::STATUS3)
-                        ->where('status', Order::STATUS5)
-                        ->whereDate('reserve_at', $date);
+                    $query->where(function($subquery) {
+                        $subquery->where('status', Order::STATUS1)
+                            ->orWhere('status', Order::STATUS3)
+                            ->orWhere('status', Order::STATUS5);
+                    })
+                    ->whereDate('reserve_at', $date);
                 })
                 ->whereHas('order_detail', function($query) use ($item) {
                     $query->where('layout_chair_id', $item->id);
