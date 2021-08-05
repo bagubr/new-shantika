cb3650db4dfe78cf471f417e13a1d9305466e1d7
misft <indrawandwiprasetyo@gmail.com>
2021-08-05 10:16:09 +0700

    [fix] search fleet

--- app/Http/Controllers/v1/Agent/RouteController.php
+++ app/Http/Controllers/v1/Agent/RouteController.php
@@ -15,17 +15,17 @@ class RouteController extends BaseRouteController
     public function getAvailableRoutes(ApiGetAvailableRouteRequest $request) {
         $user = UserRepository::findByToken($request->bearerToken());
         $agency_id = $user->agencies->id;
-        $routes = Route::with(['fleet', 'checkpoints.agency.city'])
-            ->whereHas('fleet', function($query) use ($request) {
-                $query->when(($request->fleet_class_id), function ($q) use ($request) { 
-                    $q->where('fleet_class_id', $request->fleet_class_id);
-                });
+        $routes = Route::with(['fleet', 'checkpoints.agency.city', 'checkpoints'=>function($query) {
+
+            }])
+            ->whereHas('fleet.fleetclass', function($query) use ($request) { 
+                $query->where('fleet_class_id', $request->fleet_class_id);
             })
-            ->whereHas('checkpoints', function($q) use ($request, $agency_id) {
-                $q->where(function($subquery) use ($request, $agency_id) {
-                    $subquery->where('agency_id', $request->agency_id);
-                })
-                ->orderBy('order', 'desc');
+            ->whereHas('checkpoints', function($query) use ($request, $agency_id) {
+                $query->whereRaw('exists (select 1 from checkpoints inner join checkpoints c on c.route_id = checkpoints.route_id
+                where (checkpoints.agency_id = ? and c.agency_id = ?) 
+                and (c.route_id = routes.id and checkpoints.route_id = routes.id)
+                and (checkpoints.order > c.order))', [$request->agency_id, $agency_id]);
             })
             ->when(($request->time), function ($q) use ($request) { 
                 $time_start = TimeClassificationRepository::findByName($request->time)->time_start;
@@ -37,23 +37,35 @@ class RouteController extends BaseRouteController
                 });
             })
             ->get();
+        
+        foreach($routes as $index => $route) {
+            $found_destination = false;
+            $found_departure = false;
+            $checkpoints = $route->checkpoints->filter(function($item, $key) use ($request, &$route, &$found_destination, &$found_departure, $agency_id) {
+                //
+                if($item->agency_id == $agency_id) {
+                    $route->departure_at = $item->arrived_at;
+                    $found_departure = true;
+                }
+                if(!$found_departure && !$found_destination) {
+                    return false;
+                }
 
-        foreach($routes as $route) {
-            $found = false;
-            $checkpoints = $route->checkpoints->filter(function($item, $key) use ($request, &$route, &$found) {
-                if($found) {
+
+                //
+                if($found_destination) {
                     return false;
                 }
                 if($item->agency_id == $request->agency_id) {
                     $route->arrived_at = $item->arrived_at;
-                    $found = true;
+                    $found_destination = true;
                 }
                 return true;
             });
             unset($route->checkpoints);
             $route->checkpoints = $checkpoints->values();
         }
-        
+
         return $this->sendSuccessResponse([
             'routes'=>AvailableRoutesResource::collection($routes)
         ]);
