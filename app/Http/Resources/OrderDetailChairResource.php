68315e35d0090516e042983d387d22ba9a619749
misft <indrawandwiprasetyo@gmail.com>
2021-08-06 00:59:19 +0700

    [fix] add response true price

--- app/Http/Resources/OrderDetailChairResource.php
+++ app/Http/Resources/OrderDetailChairResource.php
@@ -18,7 +18,17 @@ class OrderDetailChairResource extends JsonResource
             'chair_name'=>$this->chair->name,
             'food'=>$this->is_feed ? 1 : 0,
             'is_member'=>$this->is_member ? "Member" : "Non Member",
-            'is_travel'=>$this->is_travel ? "Travel" : "Non Travel"
+            'is_travel'=>$this->is_travel ? "Travel" : "Non Travel",
+            "price"=>$this->getTruePrice($this->order?->distribution, $this->is_food, $this->is_member, $this->is_travel)
         ];
+    }
+
+    private function getTruePrice($distribution, $is_food, $is_member, $is_travel) {
+        $order_detail_count = count($this->order?->order_detail);
+        $food = $distribution->for_food / $order_detail_count;
+        $member = $distribution->for_member / $order_detail_count;
+        $travel = $distribution->for_travel / $order_detail_count;
+
+        return $distribution->ticket_only + $food + $member + $travel;
     }
 }
