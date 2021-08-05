198c1ecd134b312a9a96092d238ba3440f62f8c7
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2021-08-05 21:40:31 +0700

    otw dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -16,6 +13,7 @@ class DashboardController extends Controller
         $orders = Order::count();
         $count_user = User::doesntHave('agencies')->count();
         $orders_money = Order::has('route')->sum('price');
+
         return view('dashboard', compact('users', 'orders', 'count_user', 'orders_money'));
     }
 }
