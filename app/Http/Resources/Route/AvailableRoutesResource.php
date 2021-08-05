8b1517c02b4c083a58ef283e6df193587060e646
misft <indrawandwiprasetyo@gmail.com>
2021-08-05 16:24:19 +0700

    [fix] availble chair count

--- app/Http/Resources/Route/AvailableRoutesResource.php
+++ app/Http/Resources/Route/AvailableRoutesResource.php
@@ -3,6 +3,10 @@
 namespace App\Http\Resources\Route;
 
 use App\Http\Resources\CheckpointStartEndResource;
+use App\Models\Fleet;
+use App\Models\LayoutChair;
+use App\Models\Order;
+use App\Models\OrderDetail;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
@@ -25,8 +29,24 @@ class AvailableRoutesResource extends JsonResource
             'arrived_at'         => $this->arrived_at,
             'fleet_class'        => $this->fleet_class,
             'price'              => $this->price,
-            'chairs_available'   => rand(1, 32),
+            'chairs_available'   => $this->getChairsAvailable($request, $this->id),
             'checkpoints'        => new CheckpointStartEndResource($this)
         ];
+    }
+
+    private function getChairsAvailable($request, $fleet_id) {
+        $used_count = OrderDetail::whereHas('order', function($query) use ($request, $fleet_id) {
+                $query->where('status', Order::STATUS_BOUGHT)
+                    ->where('reserve_at', $request->date)
+                    ->whereHas('route', function($subquery) use ($fleet_id) {
+                        $subquery->where('fleet_id', $fleet_id);
+                    });
+            })
+            ->count();
+        $total_seat = LayoutChair::whereHas('layout.fleet', function($query) use ($fleet_id) {
+            $query->where('id', $fleet_id);
+        })->count();
+
+        return $total_seat - $used_count;
     }
 }
