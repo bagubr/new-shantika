f638ced48de2bfaac787e76bff4fefebb5a918e1
Bagu bragowo <bagubragowo3@gmail.com>
2021-08-05 17:33:59 +0700

    update fix available route

--- app/Http/Resources/Route/AvailableRoutesResource.php
+++ app/Http/Resources/Route/AvailableRoutesResource.php
@@ -6,6 +6,7 @@ use App\Http\Resources\CheckpointStartEndResource;
 use App\Models\Fleet;
 use App\Models\LayoutChair;
 use App\Models\Order;
+use App\Models\Layout;
 use App\Models\OrderDetail;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
@@ -29,23 +30,24 @@ class AvailableRoutesResource extends JsonResource
             'arrived_at'         => $this->arrived_at,
             'fleet_class'        => $this->fleet_class,
             'price'              => $this->price,
-            'chairs_available'   => $this->getChairsAvailable($request, $this->id),
+            'chairs_available'   => $this->getChairsAvailable($request, $this->fleet->id, $this->id, $this->fleet->layout->id),
             'checkpoints'        => new CheckpointStartEndResource($this)
         ];
     }
 
-    private function getChairsAvailable($request, $fleet_id) {
-        $used_count = OrderDetail::whereHas('order', function($query) use ($request, $fleet_id) {
-                $query->where('status', Order::STATUS_BOUGHT)
-                    ->where('reserve_at', $request->date)
-                    ->whereHas('route', function($subquery) use ($fleet_id) {
-                        $subquery->where('fleet_id', $fleet_id);
-                    });
+    private function getChairsAvailable($request, $fleet_id, $route_id, $layout_id) {
+        $used_count = OrderDetail::whereHas('order', function($query) use ($request, $fleet_id, $route_id) {
+                $query->whereIn('status', Order::STATUS_BOUGHT);
+                $query->whereDate('reserve_at', $request->date);
+                // $query->where('route_id', $route_id);
+                // $query->whereHas('route', function($subquery) use ($fleet_id) {
+                //         $subquery->where('fleet_id', $fleet_id);
+                //     });
             })
             ->count();
-        $total_seat = LayoutChair::whereHas('layout.fleet', function($query) use ($fleet_id) {
-            $query->where('id', $fleet_id);
-        })->count();
+        $layout = Layout::find($layout_id);
+        
+        $total_seat = $layout->total_indexes - count($layout->space_indexes) - count($layout->toilet_indexes) - count($layout->door_indexes);
 
         return $total_seat - $used_count;
     }
