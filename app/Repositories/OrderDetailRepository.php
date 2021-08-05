bcc67fea873fbd6ad1c5b5abe4594b802936e3f3
misft <indrawandwiprasetyo@gmail.com>
2021-08-06 02:44:39 +0700

    [fix]

--- app/Repositories/OrderDetailRepository.php
+++ app/Repositories/OrderDetailRepository.php
@@ -12,10 +12,10 @@ class OrderDetailRepository
         return OrderDetail::where('order_id', $order)->get();
     }
 
-    public static function findForPriceDistributionByDateAndFleet($date, $fleet_id) {
+    public static function findForPriceDistributionByUserAndDateAndFleet($user_id, $date, $fleet_id) {
         $order = OrderDetail::with(['order', 'order.route.fleet', 'order.route.checkpoints', 'order.payment', 'order.distribution'])
-            ->whereHas('order', function($query) use ($date) {
-                $query->whereDate('created_at', $date)->where('status', Order::STATUS3);
+            ->whereHas('order', function($query) use ($date, $user_id) {
+                $query->whereDate('created_at', $date)->whereIn('status', [Order::STATUS3, Order::STATUS8])->where('user_id',$user_id);
             })
             ->whereHas('order.route', function($query) use ($fleet_id) {
                 $query->where('fleet_id', $fleet_id);
