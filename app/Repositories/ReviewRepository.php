ef4dbcbf34a0a2c6394c8bbaabc2312a19c30ea3
misft <indrawandwiprasetyo@gmail.com>
2021-08-06 16:55:11 +0700

    [fix] review

--- app/Repositories/ReviewRepository.php
+++ app/Repositories/ReviewRepository.php
@@ -8,7 +8,7 @@ use App\Models\Review;
 class ReviewRepository {
     public static function getHistoryOfAgent($user_id) {
         return Review::whereHas('order.route.checkpoints', function($query) use ($user_id) {
-            $query->where('order', 0)->where('agency_id', $user_id);
+            $query->where('agency_id', $user_id);
         })->get();
     }
 
