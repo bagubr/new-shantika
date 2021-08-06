090fdf9c0da76bb45ed9f891d9b0bc104204f714
misft <indrawandwiprasetyo@gmail.com>
2021-08-06 14:32:20 +0700

    [update] checkpoint destination

--- app/Http/Resources/CheckpointStartEndResource.php
+++ app/Http/Resources/CheckpointStartEndResource.php
@@ -15,6 +15,7 @@ class CheckpointStartEndResource extends JsonResource
     public function toArray($request)
     {
         $checkpoint_max_index = count($this->checkpoints) - 1;
+        $checkpoint_destination = $this->checkpoints()->where('agency_id', $this->destination_agency_id)->first();
         return [
             'start' => (object) [
                 'agency_id'=>$this->checkpoints[0]?->agency?->id ?? "",
@@ -35,6 +36,16 @@ class CheckpointStartEndResource extends JsonResource
                 'agency_lng'=>$this->checkpoints[$checkpoint_max_index]?->agency?->lng,
                 'city_name'=>$this->checkpoints[$checkpoint_max_index]?->agency?->city?->name ?? "",
                 'arrived_at'=>$this->checkpoints[$checkpoint_max_index]?->arrived_at,
+            ],
+            'destination' => (object) [
+                'agency_id'=>$checkpoint_destination->agency?->id ?? "",
+                'agency_name'=>$checkpoint_destination->agency?->name ?? "",
+                'agency_address'=>$checkpoint_destination->agency?->address ?? "",
+                'agency_phone'=>$checkpoint_destination->agency?->phone ?? "",
+                'agency_lat'=>$checkpoint_destination->agency?->lat,
+                'agency_lng'=>$checkpoint_destination->agency?->lng,
+                'city_name'=>$checkpoint_destination->agency?->city?->name ?? "",
+                'arrived_at'=>$checkpoint_destination->arrived_at,
             ]
         ];
     }
