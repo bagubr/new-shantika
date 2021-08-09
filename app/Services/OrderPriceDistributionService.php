4635c4e66c473d4e101cdc0195bd61f6ba3689ae
misft <indrawandwiprasetyo@gmail.com>
2021-08-09 20:40:33 +0700

    [fix] order price

--- app/Services/OrderPriceDistributionService.php
+++ app/Services/OrderPriceDistributionService.php
@@ -15,12 +15,10 @@ class OrderPriceDistributionService {
         $is_agent = UserRepository::findUserIsAgent($order->user_id);
         if($is_agent) {
             $total_price['for_agent'] = -1 * round($setting->commision * $total_price['for_agent']);
-            $total_price['for_owner'] = $order->route?->price + array_sum($total_price);
         } else {
             $total_price['for_agent'] = 0;
-            $total_price['for_owner'] = $order->route?->price + array_sum($total_price);
         }
-
+        $total_price['for_owner'] = array_sum($total_price);
         
         $price_distribution = OrderPriceDistribution::create(array_merge($total_price, [
             'order_id'=>$order->id,
@@ -31,27 +29,24 @@ class OrderPriceDistributionService {
     }
 
     public static function calculateDistribution($order, $order_details) {
+        $ticket_only = $order->route?->price - $order?->route?->fleet?->fleetclass?->price_food;
         $total_price = [
-            'for_food'=>0,
+            'for_food'=>$order?->route?->fleet?->fleetclass?->price_food * count($order_details),
             'for_travel'=>0,
             'for_member'=>0,
-            'for_agent'=>0, 
-            'ticket_only'=>$order->route?->price
+            'for_agent'=>$ticket_only * count($order_details),
+            'ticket_only'=>$ticket_only * count($order_details)
         ];
         $setting = Setting::first();
         foreach($order_details as $order_detail) {
-            $total_price['for_agent'] = $order->price;
-            if($order_detail->is_feed) {
-                $total_price['for_food'] += $order->route?->fleet?->fleetclass?->price_food;
-                $total_price['for_agent'] -= $order->route?->fleet?->fleetclass?->price_food; 
+            if(!$order_detail->is_feed) {
+                $total_price['for_food'] -=  $order->route?->fleet?->fleetclass?->price_food;
             }
             if($order_detail->is_travel) {
                 $total_price['for_travel'] += $setting->travel;
-                $total_price['for_agent'] -= $setting->travel;
             }
             if($order_detail->is_member) {
                 $total_price['for_member'] -= $setting->member;
-                $total_price['for_agent'] -= $setting->member;
             }
         }
 
