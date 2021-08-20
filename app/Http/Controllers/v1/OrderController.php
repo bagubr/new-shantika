1231b2b97fbc2ca8b50c8244bd46bbe9d6902f2d
misft <indrawandwiprasetyo@gmail.com>
2021-08-20 15:26:37 +0700

    [fix] route to fleet route

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApiCalculateDiscountRequest;
+use App\Models\FleetRoute;
 use App\Models\Route;
 use App\Models\Setting;
 use Illuminate\Http\Request;
@@ -11,21 +12,21 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     public function calculateDiscount(ApiCalculateDiscountRequest $request) {
-        $route = Route::find($request->route_id);
+        $fleet_route = FleetRoute::find($request->fleet_route_id);
         $setting = Setting::first();
 
-        $price_ticket = $route->price - $route->fleet?->fleetclass?->price_food;
+        $price_ticket = $fleet_route->price - $fleet_route->fleet?->fleetclass?->price_food;
         
         $data = [
             'total_food'=>$request->is_food 
-                ? $route->fleet?->fleetclass?->price_food * $request->seat_count 
+                ? $fleet_route->fleet?->fleetclass?->price_food * $request->seat_count 
                 : 0,
             'total_travel'=>$request->is_travel ? $setting->travel * $request->seat_count : 0,
             'total_member'=>$request->is_member ? $setting->member * $request->seat_count : 0
         ];
 
         $price_with_food = $request->is_food 
-            ? $route->price * $request->seat_count
+            ? $fleet_route->price * $request->seat_count
             : $price_ticket * $request->seat_count; 
         
         $data = array_merge($data, [
