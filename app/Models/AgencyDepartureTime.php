cbb18eee1ccfd6ecc9c6101ecd9b5596291b7510
misft <indrawandwiprasetyo@gmail.com>
2021-08-30 11:53:10 +0700

    [fix] agency departure time

--- app/Models/AgencyDepartureTime.php
+++ app/Models/AgencyDepartureTime.php
@@ -12,18 +12,10 @@ class AgencyDepartureTime extends Model
     protected $table = 'agency_departure_times';
 
     protected $fillable = [
-        'agency_id', 'departure_at'
-    ];
-
-    protected $appends = [
-        'time_classification_id'
+        'agency_id', 'departure_at', 'time_classification_id'
     ];
 
     public function agency() {
         return $this->belongsTo(Agency::class);
-    }
-
-    public function getTimeClassificationIdAttribute() {
-        return TimeClassification::where('time_start', '<=', $this->attributes['departure_at'])->where('time_end', '>=', $this->attributes['departure_at'])->first()?->id;
     }
 }
