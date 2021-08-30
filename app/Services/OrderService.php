63fe8065d82a98be4ebf613e39082790d7159f24
misft <indrawandwiprasetyo@gmail.com>
2021-08-31 05:20:55 +0700

    [fix] notification

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -79,8 +79,8 @@ class OrderService {
             $order->id,
             $order->user_id
         );
-        // SendingNotification::dispatch($notification, $order->user?->fcm_token, true);
-        // SendingNotification::dispatch($notification, Admin::whereNotNull('fcm_token')->pluck('fcm_token'), false);
+        SendingNotification::dispatch($notification, $order->user?->fcm_token, true);
+        SendingNotification::dispatch($notification, Admin::whereNotNull('fcm_token')->pluck('fcm_token'), false);
     }
 
     public static function createDetail($order, $layout_chairs, $detail) {
