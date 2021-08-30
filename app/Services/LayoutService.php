814bde810193b49053bd1c0b52a2b52a524d4441
misft <indrawandwiprasetyo@gmail.com>
2021-08-30 11:36:35 +0700

    [fix] sketch add code agency

--- app/Services/LayoutService.php
+++ app/Services/LayoutService.php
@@ -47,20 +47,21 @@ class LayoutService {
         $layout->chairs = $layout->chairs->map(function ($item) use ($fleet_route, $date, $layout, $unavailable, $booking, $user_id) {
             $item->is_booking = $booking->where('layout_chair_id', $item->id)->isNotEmpty();
             $item->is_unavailable = $unavailable->filter(function($e) use ($item) {
-                return $e->order_detail->where('layout_chair_id', $item->id)->first();
-            })->isNotEmpty();
-            $item->is_mine = $unavailable->filter(function($e) use ($item, $user_id) {
-                return $e->order_detail->where('layout_chair_id', $item->id)->isNotEmpty() && $e->user_id == $user_id && $e->user_id != null;
+                return $e->order_detail->where('layout_chair_id', $item->id)->isNotEmpty();
             })->isNotEmpty();
             if($item->is_booking) {
                 $item->booking_detail = $booking->filter(function($value) use ($item) {
                     return $value->layout_chair_id == $item->id;
                 })->first();
+                $item->code = $booking->where('layout_chair_id', $item->id)->first()->agency->code;
             }
             if($item->is_unavailable) {
                 $item->order_detail = $unavailable->filter(function($value) use ($item) {
                     return $value->order_detail->where('layout_chair_id', $item->id)->isNotEmpty();
                 })->first();
+                $item->code = $unavailable->filter(function($value) use ($item) {
+                    return $value->order_detail->where('layout_chair_id', $item->id)->isNotEmpty();
+                })->first()?->agency?->code;
             }
             return $item;
         });
