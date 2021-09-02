ce610158430bc12b92a81db73e9ccde1df8867f9
satriotol <satriotol69@gmail.com>
2021-09-02 11:02:03 +0700

    col

--- resources/views/order_price_distribution/index.blade.php
+++ resources/views/order_price_distribution/index.blade.php
@@ -77,187 +77,195 @@ Setoran
             </div>
             <div class="col-8">
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-md-6">
                         <div class="small-box bg-success">
                             <div class="inner">
                                 <h3><sup style="font-size: 20px">Rp</sup> {{number_format($count_income)}}</h3>
                                 <p>Total Pemasukkan</p>
                             </div>
                             <div class="icon">
-                                <i class="ion ion-stats-bars"></i>
+                                <i class="ion ion-card"></i>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6">
+                    <div class="col-md-6">
                         <div class="small-box bg-danger">
                             <div class="inner">
                                 <h3><sup style="font-size: 20px">Rp</sup> {{number_format($count_outcome)}}</h3>
                                 <p>Total Pengeluaran</p>
                             </div>
                             <div class="icon">
-                                <i class="ion ion-stats-bars"></i>
+                                <i class="ion ion-card"></i>
                             </div>
                         </div>
                     </div>
-                    <div class="col-12">
-                        <div class="small-box bg-warning">
+                    {{-- <div class="col-md-4">
+                        <div class="small-box bg-primary">
                             <div class="inner">
-                                <h3><sup style="font-size: 20px">Rp</sup> {{number_format($count_pendapatan_bersih)}}
-                                </h3>
-                                <p>Total Pendapatan Bersih</p>
-                            </div>
-                            <div class="icon">
-                                <i class="ion ion-stats-bars"></i>
-                            </div>
-                        </div>
-                    </div>
+                                <h3>{{number_format($count_seat)}} <sup style="font-size: 20px">Kursi</sup>
+                    </h3>
+                    <p>Total Kursi Terjual</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-man"></i>
                 </div>
             </div>
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Table Setoran</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Tanggal Pemesanan</th>
-                                    <th>Kode Order</th>
-                                    <th>Armada</th>
-                                    <th>Agen</th>
-                                    <th>Jumlah Seat</th>
-                                    <th>Rute</th>
-                                    <th>Harga Tiket</th>
-                                    <th>Dana Agen</th>
-                                    <th>Makan</th>
-                                    <th>Travel</th>
-                                    <th>Member</th>
-                                    <th>Agent</th>
-                                    <th>Total Owner</th>
-                                    <th>Deposit</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($order_price_distributions as $order_price_distribution)
-                                <tr>
-                                    <td>{{date('Y-m-d',strtotime($order_price_distribution->order?->reserve_at))}}</td>
-                                    <td>
-                                        @if ($order_price_distribution->order)
-                                        <a href="{{route('order.show',$order_price_distribution->order?->id)}}">
-                                            {{$order_price_distribution->order?->code_order}}
-                                        </a>
-                                        @endif
-                                    </td>
-                                    <td>{{$order_price_distribution->order?->fleet_route?->fleet_detail?->fleet?->name}}/{{$order_price_distribution->order?->fleet_route?->fleet_detail?->fleet?->fleetclass?->name}}
-                                        ({{$order_price_distribution->order?->fleet_route?->fleet_detail?->nickname}})
-                                    </td>
-                                    <td>{{$order_price_distribution->order?->agency?->name}}</td>
-                                    <td>
-                                        {{$order_price_distribution->order?->order_detail?->count()}}
-                                        (
-                                        @foreach ($order_price_distribution->order?->order_detail as $order_detail)
-                                        {{$order_detail->chair?->name}}
-                                        @if (!$loop->last)
-                                        ,
-                                        @endif
-                                        @endforeach
-                                        )
-                                    </td>
-                                    <td>
-                                        @if ($order_price_distribution->order?->fleet_route)
-                                        <a
-                                            href="{{route('fleet_route.show',$order_price_distribution->order?->fleet_route_id)}}">
-                                            {{$order_price_distribution->order?->fleet_route?->route?->name}}
-                                        </a>
-                                        @endif
-                                    </td>
-                                    <td>Rp. {{number_format($order_price_distribution->order?->fleet_route?->price)}}
-                                    </td>
-                                    <td>Rp.
-                                        {{number_format($order_price_distribution->order?->fleet_route?->price * $order_price_distribution->order?->order_detail?->count())}}
-                                    </td>
-                                    <td>Rp. {{number_format($order_price_distribution->for_food,2)}}</td>
-                                    <td>Rp. {{number_format($order_price_distribution->for_travel,2)}}</td>
-                                    <td>Rp. {{number_format($order_price_distribution->for_member,2)}}</td>
-                                    <td>Rp. {{number_format($order_price_distribution->for_agent,2)}}</td>
-                                    <td>Rp. {{number_format($order_price_distribution->for_owner,2)}}</td>
-                                    <td>
-                                        @if ($order_price_distribution->deposited_at)
-                                        {{date('Y-m-d', strtotime($order_price_distribution->deposited_at))}}
-                                        @else
-                                        Belum Deposit
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if (!$order_price_distribution->deposited_at)
-                                        <form
-                                            action="{{route('order_price_distribution.update', $order_price_distribution->id)}}"
-                                            class="d-inline" method="POST">
-                                            @csrf
-                                            @method('PUT')
-                                            <button class="btn btn-primary btn-xs"
-                                                onclick="return confirm('Apakah Anda Yakin Ingin Deposit?')"
-                                                type="submit">Deposit
-                                                Sekarang</button>
-                                        </form>
-                                        @endif
-                                        <form
-                                            action="{{route('order_price_distribution.destroy',$order_price_distribution->id)}}"
-                                            class="d-inline" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-danger btn-xs"
-                                                onclick="return confirm('Apakah Anda Yakin  Menghapus Data Ini??')"
-                                                type="submit">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
+        </div> --}}
+        <div class="col-md-12">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3><sup style="font-size: 20px">Rp</sup> {{number_format($count_pendapatan_bersih)}}
+                    </h3>
+                    <p>Total Pendapatan Bersih</p>
                 </div>
-                <!-- /.card -->
-            </div>
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Table Pengeluaran</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example2" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Tanggal Laporan</th>
-                                    <th>Rute/Armada</th>
-                                    <th>Nama Pengeluaran</th>
-                                    <th>Nominal Pengeluaran</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($outcome_details as $outcome)
-                                <tr>
-                                    <td>{{date('Y-m-d',strtotime($outcome->outcome?->reported_at))}}</td>
-                                    <td>{{$outcome->outcome?->order_price_distributions ?? 'Tidak Ada'}}</td>
-                                    <td>{{$outcome->name}}</td>
-                                    <td>Rp {{number_format($outcome->amount)}}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
                 </div>
-                <!-- /.card -->
             </div>
-            <!-- /.col -->
         </div>
     </div>
+</div>
+<div class="col-12">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Table Setoran</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Tanggal Pemesanan</th>
+                        <th>Kode Order</th>
+                        <th>Armada</th>
+                        <th>Agen</th>
+                        <th>Jumlah Seat</th>
+                        <th>Rute</th>
+                        <th>Harga Tiket</th>
+                        <th>Dana Agen</th>
+                        <th>Makan</th>
+                        <th>Travel</th>
+                        <th>Member</th>
+                        <th>Agent</th>
+                        <th>Total Owner</th>
+                        <th>Deposit</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($order_price_distributions as $order_price_distribution)
+                    <tr>
+                        <td>{{date('Y-m-d',strtotime($order_price_distribution->order?->reserve_at))}}</td>
+                        <td>
+                            @if ($order_price_distribution->order)
+                            <a href="{{route('order.show',$order_price_distribution->order?->id)}}">
+                                {{$order_price_distribution->order?->code_order}}
+                            </a>
+                            @endif
+                        </td>
+                        <td>{{$order_price_distribution->order?->fleet_route?->fleet_detail?->fleet?->name}}/{{$order_price_distribution->order?->fleet_route?->fleet_detail?->fleet?->fleetclass?->name}}
+                            ({{$order_price_distribution->order?->fleet_route?->fleet_detail?->nickname}})
+                        </td>
+                        <td>{{$order_price_distribution->order?->agency?->name}}</td>
+                        <td>
+                            {{$order_price_distribution->order?->order_detail?->count()}}
+                            (
+                            @foreach ($order_price_distribution->order?->order_detail as $order_detail)
+                            {{$order_detail->chair?->name}}
+                            @if (!$loop->last)
+                            ,
+                            @endif
+                            @endforeach
+                            )
+                        </td>
+                        <td>
+                            @if ($order_price_distribution->order?->fleet_route)
+                            <a href="{{route('fleet_route.show',$order_price_distribution->order?->fleet_route_id)}}">
+                                {{$order_price_distribution->order?->fleet_route?->route?->name}}
+                            </a>
+                            @endif
+                        </td>
+                        <td>Rp. {{number_format($order_price_distribution->order?->fleet_route?->price)}}
+                        </td>
+                        <td>Rp.
+                            {{number_format($order_price_distribution->order?->fleet_route?->price * $order_price_distribution->order?->order_detail?->count())}}
+                        </td>
+                        <td>Rp. {{number_format($order_price_distribution->for_food,2)}}</td>
+                        <td>Rp. {{number_format($order_price_distribution->for_travel,2)}}</td>
+                        <td>Rp. {{number_format($order_price_distribution->for_member,2)}}</td>
+                        <td>Rp. {{number_format($order_price_distribution->for_agent,2)}}</td>
+                        <td>Rp. {{number_format($order_price_distribution->for_owner,2)}}</td>
+                        <td>
+                            @if ($order_price_distribution->deposited_at)
+                            {{date('Y-m-d', strtotime($order_price_distribution->deposited_at))}}
+                            @else
+                            Belum Deposit
+                            @endif
+                        </td>
+                        <td>
+                            @if (!$order_price_distribution->deposited_at)
+                            <form action="{{route('order_price_distribution.update', $order_price_distribution->id)}}"
+                                class="d-inline" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <button class="btn btn-primary btn-xs"
+                                    onclick="return confirm('Apakah Anda Yakin Ingin Deposit?')" type="submit">Deposit
+                                    Sekarang</button>
+                            </form>
+                            @endif
+                            <form action="{{route('order_price_distribution.destroy',$order_price_distribution->id)}}"
+                                class="d-inline" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger btn-xs"
+                                    onclick="return confirm('Apakah Anda Yakin  Menghapus Data Ini??')"
+                                    type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
+<div class="col-12">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Table Pengeluaran</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <table id="example2" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Tanggal Laporan</th>
+                        <th>Rute/Armada</th>
+                        <th>Nama Pengeluaran</th>
+                        <th>Nominal Pengeluaran</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($outcome_details as $outcome)
+                    <tr>
+                        <td>{{date('Y-m-d',strtotime($outcome->outcome?->reported_at))}}</td>
+                        <td>{{$outcome->outcome?->order_price_distributions ?? 'Tidak Ada'}}</td>
+                        <td>{{$outcome->name}}</td>
+                        <td>Rp {{number_format($outcome->amount)}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
+<!-- /.col -->
+</div>
+</div>
 </div>
 @endsection
 @push('script')
