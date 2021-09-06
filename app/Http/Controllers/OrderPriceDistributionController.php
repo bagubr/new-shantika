298e2487a13726be3d27a96c3ae6e678cf03d9d4
Bagu bragowo <bagubragowo3@gmail.com>
2021-09-06 11:05:33 +0700

    array

--- app/Http/Controllers/OrderPriceDistributionController.php
+++ app/Http/Controllers/OrderPriceDistributionController.php
@@ -98,40 +98,6 @@ class OrderPriceDistributionController extends Controller
 
     public function export(Request $request)
     {
-        $fleet_detail_id = $request->fleet_detail_id;
-        $date_search = $request->date_search;
-        $agency_id = $request->agency_id;
-
-        $order_price_distributions = OrderPriceDistribution::query();
-
-        if (!empty($fleet_detail_id)) {
-            $order_price_distributions = $order_price_distributions->whereHas('order', function ($q) use ($fleet_detail_id) {
-                $q->whereIn('status', ['PAID', 'EXCHANGED', 'FINSIHED'])->whereHas('fleet_route', function ($sq) use ($fleet_detail_id) {
-                    $sq->where('fleet_detail_id', $fleet_detail_id);
-                });
-            });
-            $outcome_details = $outcome_details->whereHas('outcome', function ($q) use ($fleet_detail_id) {
-                $q->where('fleet_detail_id', $fleet_detail_id);
-            });
-        }
-        if (!empty($date_search)) {
-            $order_price_distributions = $order_price_distributions->whereHas('order', function ($q) use ($date_search) {
-                $q->where('reserve_at', $date_search)->whereIn('status', ['PAID', 'EXCHANGED', 'FINSIHED']);
-            });
-            $outcome_details = $outcome_details->whereHas('outcome', function ($q) use ($date_search) {
-                $q->where('reported_at', $date_search);
-            });
-        }
-
-        if (!empty($agency_id)) {
-            $order_price_distributions = $order_price_distributions->whereHas('order', function ($q) use ($agency_id) {
-                $q->where('departure_agency_id', $agency_id)->whereIn('status', ['PAID', 'EXCHANGED', 'FINSIHED']);
-            });
-        }
-        $order_price_distributions = $order_price_distributions->get();
-        return view('excel_export.setoran', [
-            'order_price_distributions' => $order_price_distributions,
-        ]);
         return Excel::download(new SetoranExport($request), date('dmYHis').'.xlsx');
     }
     /**
