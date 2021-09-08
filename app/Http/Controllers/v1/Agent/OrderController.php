44f84ec6f3e901275dcdcafb3de746603a4b67be
Bagu bragowo <bagubragowo3@gmail.com>
2021-09-08 14:57:49 +0700

    fix bugh

--- app/Http/Controllers/v1/Agent/OrderController.php
+++ app/Http/Controllers/v1/Agent/OrderController.php
@@ -104,7 +104,7 @@ class OrderController extends Controller
 
     public function showSetoran(Request $request) {
         $user = UserRepository::findByToken($request->bearerToken());
-        $chairs = OrderDetailRepository::findForPriceDistributionByUserAndDateAndFleet($user->id,$request->date, $request->fleet_id);        
+        $chairs = OrderDetailRepository::findForPriceDistributionByUserAndDateAndFleet($user->id,$request->date, $request->fleet_id);
         $order = OrderRepository::findForPriceDistributionByDateAndFleet($user->id,$request->date, $request->fleet_id);
         if($chairs->isEmpty() && $order->isEmpty()) {
             $this->sendSuccessResponse([
