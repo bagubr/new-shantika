86ee001d77b8ff4802f710c56a1dc4779e9e01ce
misft <indrawandwiprasetyo@gmail.com>
2021-09-10 06:16:12 +0700

    [fix] langsir

--- app/Http/Controllers/SketchController.php
+++ app/Http/Controllers/SketchController.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\LangsirExport;
 use App\Repositories\OrderDetailRepository;
-use Mpdf\Mpdf;
+use PDF;
 
 class SketchController extends Controller
 {
@@ -27,7 +27,7 @@ class SketchController extends Controller
     {
         $areas = Area::get();
         return view('sketch.index_1', [
-            'areas'=>$areas
+            'areas' => $areas
         ]);
     }
 
@@ -42,8 +42,8 @@ class SketchController extends Controller
             ->when($date, function ($query) use ($date) {
                 $query->whereDate('reserve_at', $date);
             })
-            ->when($area_id, function($query) use ($area_id) {
-                $query->whereHas('fleet_route.route.checkpoints', function($subquery) use ($area_id) {
+            ->when($area_id, function ($query) use ($area_id) {
+                $query->whereHas('fleet_route.route.checkpoints', function ($subquery) use ($area_id) {
                     $subquery->whereHas('agency.city', function ($subsubquery) use ($area_id) {
                         $subsubquery->where('area_id', '!=', $area_id);
                     });
@@ -51,42 +51,44 @@ class SketchController extends Controller
             })
             ->distinct('fleet_route_id')
             ->get();
-        foreach($orders as $order) {
-            $order->order_detail_count = count(OrderDetailRepository::findForPriceDistributionByUserAndDateAndFleet(null,$order->reserve_at, $order->fleet_route?->fleet_detail?->fleet_id));
+        foreach ($orders as $order) {
+            $order->order_detail_count = count(OrderDetailRepository::findForPriceDistributionByUserAndDateAndFleet(null, $order->reserve_at, $order->fleet_route?->fleet_detail?->fleet_id));
         }
         return response([
-            'orders'=>$orders
+            'orders' => $orders
         ]);
     }
 
-    public function getAvailibilityChairs(Request $request) {
+    public function getAvailibilityChairs(Request $request)
+    {
         $fleet_route = FleetRoute::find($request->fleet_route_id);
         $layout = LayoutRepository::findByFleetRoute($fleet_route);
         $layout = LayoutService::getAvailibilityChairsDetail($layout, $fleet_route, $request->date);
         $this->sendSuccessResponse([
-            'data'=> new LayoutResource($layout),
-            'fleet'=>$fleet_route->fleet_detail?->fleet?->load('fleetclass')
+            'data' => new LayoutResource($layout),
+            'fleet' => $fleet_route->fleet_detail?->fleet?->load('fleetclass')
         ]);
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $froms = $request->data['from_layout_chair_id'];
         $tos = $request->data['to_layout_chair_id'];
 
         DB::beginTransaction();
         $detail = [];
-        foreach($froms as $key => $value) {
-            $detail[$key] = OrderDetail::whereHas('order', function($query) use ($request) {
+        foreach ($froms as $key => $value) {
+            $detail[$key] = OrderDetail::whereHas('order', function ($query) use ($request) {
                 $query->whereDate('reserve_at', date('Y-m-d', strtotime($request->data['from_date'])))->where('fleet_route_id', $request['first_fleet_route_id']);
             })->where('layout_chair_id', $value['id'])->first();
             $detail[$key]->update([
-                'layout_chair_id'=>$tos[$key]['id']
+                'layout_chair_id' => $tos[$key]['id']
             ]);
         }
-        foreach($froms as $key => $value) {
+        foreach ($froms as $key => $value) {
             $detail[$key]->order()->update([
-                'fleet_route_id'=>$request['second_fleet_route_id'],
-                'reserve_at'=>$request->data['to_date']
+                'fleet_route_id' => $request['second_fleet_route_id'],
+                'reserve_at' => $request->data['to_date']
             ]);
             $detail[$key]->refresh();
             $notification = Notification::build(
@@ -96,7 +98,7 @@ class SketchController extends Controller
                 $detail[$key]->order->id,
                 $detail[$key]->order->user_id
             );
-            SendingNotification::dispatch($notification, $detail[$key]->order?->user?->fcm_token,true);
+            SendingNotification::dispatch($notification, $detail[$key]->order?->user?->fcm_token, true);
         }
         DB::commit();
         session()->flash('success', 'Kursi penumpang berhasil diubah');
@@ -108,44 +110,34 @@ class SketchController extends Controller
         $area_id = $request->area_id;
         $date = $request->date;
         $fleet_route_id = $request->fleet_route_id;
-        $data['langsir'] = Order::whereIn('status', Order::STATUS_BOUGHT)
-        ->with('fleet_route.fleet_detail.fleet.fleetclass', 'fleet_route.route')
-        ->with('fleet_route.fleet_detail.fleet.layout')
-        ->withCount(['order_detail'=>function($query) {
-            $query->whereHas('order', function($subquery) {
-                $subquery->whereRaw('fleet_route_id = orders.fleet_route_id');
-            });
-        }])
-        ->when($date, function ($query) use ($date) {
-            $query->whereDate('reserve_at', $date);
-        })
-        ->when($area_id, function($query) use ($area_id) {
-            $query->whereHas('fleet_route.route.checkpoints', function($subquery) use ($area_id) {
-                $subquery->whereHas('agency.city', function ($subsubquery) use ($area_id) {
-                    $subsubquery->where('area_id', '!=', $area_id);
+        $data['langsir'] = OrderDetail::whereHas('order',function($q) use($date, $area_id, $fleet_route_id) {
+            $q->whereIn('status', Order::STATUS_BOUGHT)
+            ->when($date, function ($query) use ($date) {
+                $query->whereDate('reserve_at', $date);
+            })
+            ->when($area_id, function($query) use ($area_id) {
+                $query->whereHas('fleet_route.route.checkpoints', function($subquery) use ($area_id) {
+                    $subquery->whereHas('agency.city', function ($subsubquery) use ($area_id) {
+                        $subsubquery->where('area_id', '!=', $area_id);
+                    });
                 });
+            })->when($fleet_route_id, function ($query) use ($fleet_route_id)
+            {
+                $query->where('fleet_route_id', $fleet_route_id);
             });
-        })->when($fleet_route_id, function ($query) use ($fleet_route_id)
-        {
-            $query->where('fleet_route_id', $fleet_route_id);
         })
-        ->get();
-        $data['agencies'] = Agency::whereHas('city', function ($query) use ($area_id)
-        {
+        ->with('order.fleet_route.fleet_detail.fleet.fleetclass', 'order.fleet_route.route')
+        ->with('order.fleet_route.fleet_detail.fleet.layout', 'chair')
+        ->get()
+        ->sortBy('chair.name');
+        $data['agencies'] = Agency::whereHas('city', function ($query) use ($area_id) {
             $query->whereAreaId($area_id);
         })->get();
 
         $data['date'] = $date;
         $data['fleet_route_id'] = $fleet_route_id;
-
-        $pdf = Mpdf::loadView('excel_export.langsir', $data);
+        
+        $pdf = \PDF::loadView('excel_export.langsir', $data);
         $pdf->stream('document.pdf');
-
-        // $mpdf = new \Mpdf\Mpdf();
-        // $mpdf->loadView('excel_export.langsir');
-        // $mpdf->Output();
-        // $file_name = date('dmYHis');
-        // return Excel::download(new LangsirExport($request), $file_name.'.pdf', Excel::MPDF);
     }
-    
 }
--- resources/views/excel_export/langsir.blade.php
+++ resources/views/excel_export/langsir.blade.php
@@ -25,24 +25,16 @@
         </tr>
     </thead>
     <tbody>
-        @php
-        $index = 0;
-        @endphp
         @foreach($agencies as $agency)
-            @foreach($langsir as $order)
-                @if($agency->id == $order->departure_agency_id)
-                    @foreach($order->order_detail as $order_detail)
-                    @php
-                        $layout_chair_exists[] = $order_detail->layout_chair_id;
-                    @endphp
+            @foreach($langsir as $order_detail)
+                @if($agency->id == $order_detail->order->departure_agency_id)
                     <tr>
                         <td width="5%">{{$loop->iteration}}</td>
                         <td>{{$agency->name}}</td>
-                        <td width="20%">({{\App\Models\LayoutChair::find($order_detail->layout_chair_id)->name}}),</td>
+                        <td width="20%">({{$order_detail->chair->name}}),</td>
                         <td width="20%">{{ $order_detail->name }},</td>
-                        <td width="20%">{{ \App\Models\Agency::find($order->destination_agency_id)->name }}</td>
+                        <td width="20%">{{ $order_detail->order->agency_destiny->name }}</td>
                     </tr>
-                    @endforeach
                 @endif
             @endforeach
         @endforeach
@@ -50,7 +42,8 @@
             <th>Bangku Kosong</th>
             <th colspan="4">
                 @php
-                 $layout_chair = \App\Models\LayoutChair::whereLayoutId(\App\Models\FleetRoute::find($fleet_route_id)->fleet_detail?->fleet?->layout_id)->get()->pluck('id');
+                 $layout_chair_exists = $order_detail->pluck('layout_chair_id')->toArray();
+                 $layout_chair = \App\Models\LayoutChair::whereLayoutId(\App\Models\FleetRoute::find($fleet_route_id)->fleet_detail?->fleet?->layout_id)->orderBy('name', 'asc')->get()->pluck('id');
                 @endphp
                 @foreach($layout_chair as $layout_chair_id)
                     @if(!in_array($layout_chair_id, $layout_chair_exists))
