28d66d561ff6f154e0c32f3c5e65bf4aa775f2b3
misft <indrawandwiprasetyo@gmail.com>
2021-09-10 13:54:40 +0700

    [fix]

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -31,7 +31,7 @@ class OrderController extends Controller
         
         $data = array_merge($data, [
             'price_ticket'=>$price_ticket,
-            'total_price'=>$price_ticket + $data['total_travel'] + $data['total_member'] + ($request->is_food ? $data['for_food'] : $setting->default_food_price * $request->seat_count)
+            'total_price'=>$price_ticket + $data['total_travel'] + $data['total_member'] + ($request->is_food ? $data['total_food'] : $setting->default_food_price * $request->seat_count)
         ]);
 
         return $this->successResponse($data);
