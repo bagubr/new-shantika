01923fe41cb26b35796b6d8ea5a9481e780ce3f7
misft <indrawandwiprasetyo@gmail.com>
2021-09-15 14:25:31 +0700

    [update] api slider

--- app/Http/Controllers/v1/SliderController.php
+++ app/Http/Controllers/v1/SliderController.php
@@ -8,6 +8,13 @@ use App\Repositories\SliderRepository;
 
 class SliderController extends Controller
 {
+    public function index() {
+        $data = SliderRepository::getSliderCust();
+
+        $this->sendSuccessResponse([
+            'slider'=>$data
+        ]);
+    }
     public function sliderDetail($id)
     {
         $data = SliderRepository::findById($id);
