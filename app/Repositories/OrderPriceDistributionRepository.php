e5d77b288a35d1fe92385fa2f469d82053676092
misft <indrawandwiprasetyo@gmail.com>
2021-09-17 14:21:25 +0700

    [fix] query

--- app/Repositories/OrderPriceDistributionRepository.php
+++ app/Repositories/OrderPriceDistributionRepository.php
@@ -46,14 +46,16 @@ class OrderPriceDistributionRepository
         $user = UserRepository::findByToken($token);
         $sum = OrderPriceDistribution::whereHas('order', function ($query) use ($user, $date) {
             $query->where(function($subquery) use ($user) {
-                $subquery->where('departure_agency_id', $user->agencies?->agent?->id)
+                $subquery->where(function($subsubquery) use ($user) {
+                    $subsubquery->where('departure_agency_id', $user->agencies?->agent?->id)
                     ->whereHas('user.agencies')
                     ->whereIn('status', [Order::STATUS3]);
-            })
-            ->orWhere(function($subquery) use ($user) {
-                $subquery->where('departure_agency_id', $user->agencies?->agent?->id)
-                ->whereDoesntHave('user.agencies')
-                ->whereIn('status', [Order::STATUS5, Order::STATUS8]);
+                })
+                ->orWhere(function($subsubquery) use ($user) {
+                    $subsubquery->where('departure_agency_id', $user->agencies?->agent?->id)
+                    ->whereDoesntHave('user.agencies')
+                    ->whereIn('status', [Order::STATUS5, Order::STATUS8]);
+                });
             })
             ->whereDate('reserve_at', $date);
         })->sum('for_agent');
