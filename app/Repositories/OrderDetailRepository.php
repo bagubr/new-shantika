38ebc5e9c73bb00080c57503bdb6a946b6dbc55f
misft <indrawandwiprasetyo@gmail.com>
2021-09-29 09:53:39 +0700

    [fix]

--- app/Repositories/OrderDetailRepository.php
+++ app/Repositories/OrderDetailRepository.php
@@ -45,12 +45,11 @@ class OrderDetailRepository
     }
 
     public static function getAllByAgencyId(User $user, $date) {
-        $user_order =  OrderDetail::with(['order.distribution', 'chair'=>function($query) {
-            $query->orderBy('name', 'asc');
-        }])->whereHas('order', function($query) use ($user, $date) {
+        $user_order =  OrderDetail::with(['order.distribution', 'chair'])->whereHas('order', function($query) use ($user, $date) {
             $query->where('departure_agency_id', $user->agencies->agency_id)
             ->whereDate('reserve_at', $date);
         })
+        ->orderBy('layout_chair_id', 'asc')
         ->get();
         return $user_order;
     }
