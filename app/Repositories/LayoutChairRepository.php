a51b31cbb56e4236a3cbcec5c414125f908164df
misft <indrawandwiprasetyo@gmail.com>
2021-10-09 05:56:47 +0700

    [fix] filter

--- app/Repositories/LayoutChairRepository.php
+++ app/Repositories/LayoutChairRepository.php
@@ -10,15 +10,17 @@ class LayoutChairRepository {
         $agency_id = UserRepository::findByToken($token)?->agencies->agent->id;
 
         return OrderDetail::whereHas('order', function($query) use ($agency_id, $date) {
-            $query->where(function($subsubquery) use ($agency_id) {
-                $subsubquery->where('departure_agency_id', $agency_id)
-                    ->whereHas('user.agencies')
-                    ->whereIn('status', [Order::STATUS3]);
-            })
-            ->orWhere(function($subsubquery) use ($agency_id) {
-                $subsubquery->where('departure_agency_id', $agency_id)
-                ->whereDoesntHave('user.agencies')
-                ->whereIn('status', [Order::STATUS5, Order::STATUS8]);
+            $query->where(function($subquery) use ($agency_id) {
+                $subquery->where(function($subsubquery) use ($agency_id) {
+                    $subsubquery->where('departure_agency_id', $agency_id)
+                        ->whereHas('user.agencies')
+                        ->whereIn('status', [Order::STATUS3]);
+                })
+                ->orWhere(function($subsubquery) use ($agency_id) {
+                    $subsubquery->where('departure_agency_id', $agency_id)
+                    ->whereDoesntHave('user.agencies')
+                    ->whereIn('status', [Order::STATUS5, Order::STATUS8]);
+                });
             })
             ->whereDate('reserve_at', $date);
         })
