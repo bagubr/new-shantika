345d3031646ee6da41ae82bd3ad46ea908c0adc9
misft <indrawandwiprasetyo@gmail.com>
2021-10-12 05:20:38 +0700

    [fix] pricing

--- app/Services/OrderPriceDistributionService.php
+++ app/Services/OrderPriceDistributionService.php
@@ -36,6 +36,7 @@ class OrderPriceDistributionService {
                 : $order->agency_destiny->prices->sortByDesc('start_at')->first()->price, 
             'food'=>0
         ];
+        $total_price['ticket_price'] += $order->fleet_route->prices[0]->true_deviation_price;
         $total_price['for_agent'] = $total_price['ticket_only'] * $setting->commision;
         
         foreach($order_details as $order_detail) {
