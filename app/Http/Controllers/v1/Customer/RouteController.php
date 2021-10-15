569f250ae1bb457e360b745aae06e952333fafc2
misft <indrawandwiprasetyo@gmail.com>
2021-10-15 14:46:31 +0700

    [fix]

--- app/Http/Controllers/v1/Customer/RouteController.php
+++ app/Http/Controllers/v1/Customer/RouteController.php
@@ -34,8 +34,6 @@ class RouteController extends Controller
 
         $routes = FleetRoute::with(['fleet_detail.fleet.layout', 'route.checkpoints.agency.city', 'route.checkpoints.agency.prices'=>function($query) {
             $query->orderBy('id', 'desc');
-        }, 'prices'=>function($query) {
-            $query->orderBy('id', 'desc');
         }])
         ->where('is_active', true)
         ->whereHas('fleet_detail', function($query) use ($request) {
