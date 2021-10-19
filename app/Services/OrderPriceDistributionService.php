def15ebde5a38eb26ee0fc7973bb9583af4525bf
misft <indrawandwiprasetyo@gmail.com>
2021-10-19 14:49:44 +0700

    [fix]

--- app/Services/OrderPriceDistributionService.php
+++ app/Services/OrderPriceDistributionService.php
@@ -36,13 +36,14 @@ class OrderPriceDistributionService {
                 : $order->agency_destiny->prices->sortByDesc('created_at')->first()->price, 
             'food'=>0
         ];
+        $total_price['ticket_price'] += $order->fleet_route->prices[0]->true_deviation_price;
+        $total_price['for_agent'] = $total_price['ticket_only'] * $setting->commision;
         
         foreach($order_details as $order_detail) {
             if($order_detail->is_feed) {
                 $total_price['for_food'] +=  $price_food;
                 $total_price['food'] += $price_food;
             } else {
-                $total_price['ticket_only'] -= $setting->default_food_price;
                 $total_price['food'] += $price_food - $setting->default_food_price;
             }
             if($order_detail->is_travel) {
@@ -52,13 +53,6 @@ class OrderPriceDistributionService {
                 $total_price['for_member'] -= $setting->member;
             }
         }
-
-        $total_price['ticket_price'] += $order->fleet_route->prices()->whereDate('start_at', '<=', $order->reserve_at)
-        ->whereDate('end_at', '>=', $order->reserve_at)
-        ->orderBy('id', 'desc')
-        ->first()->true_deviation_price;
-        
-        $total_price['for_agent'] = $total_price['ticket_only'] * $setting->commision;
 
         $is_agent = UserRepository::findUserIsAgent($order->user_id);
         if(!$is_agent && $order->status == Order::STATUS1) {
