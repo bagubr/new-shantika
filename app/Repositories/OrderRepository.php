a000201d41b88fe48740586c8a9c489f7e1492cf
misft <indrawandwiprasetyo@gmail.com>
2021-10-29 10:03:37 +0700

    [fix]

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -121,11 +121,10 @@ class OrderRepository
                 });
             })
             ->with(['fleet_route.fleet_detail.fleet'])
+            ->withCount(['distribution as total_deposit'])
             ->distinct('fleet_route_id')
             ->whereDate('reserve_at', $date)
             ->orderBy('fleet_route_id', 'asc')
-            ->select()
-            ->addSelect(DB::raw("(select sum(total_deposit) from order_price_distributions opd left join orders o on o.id = opd.order_id where o.fleet_route_id = orders.fleet_route_id and o.reserve_at::text ilike '%$date%' and o.departure_agency_id = $agency_id) as total_deposit_fleet_route"))
             ->get();
             // ->groupBy('fleet_route.fleet_detail.fleet_id')
             // ->all();
