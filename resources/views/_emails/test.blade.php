fe1c2aaffeb7c257d0a4305264796058f8c2de48
Bagu bragowo <bagubragowo3@gmail.com>
2022-01-10 11:36:52 +0700

    smtp non tls

---
+++ resources/views/_emails/test.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        * {
+            font-family: 'Helvetica', sans-serif; 
+        }
+        .table-collapse {
+            border-collapse: collapse;
+        }
+        .border-black {
+            border: 1px solid black;
+        }
+        th {
+            font-weight: bold;
+            border: 1px solid black;
+            padding-left: 8px;
+            padding-right: 8px;
+            padding-bottom: 4px;
+            padding-top: 4px;
+        }
+        td {
+            border: 1px solid black;
+            padding-left: 8px;
+            padding-right: 8px;
+            padding-bottom: 4px;
+            padding-top: 4px;
+            font-weight: normal;
+        }
+    </style>
+</head>
+<body>
+    Terima kasih, anda telah melakukan pemesanan pada vendor kami!
+    <br>
+    <p>Berikut adalah detail pesanan anda:</p>
+    <br>
+    Terima kasih telah memesan melalui aplikasi kami semoga perjalanan anda menyenangkan dan selamat sampai tujuan!
+</body>
+</html>
