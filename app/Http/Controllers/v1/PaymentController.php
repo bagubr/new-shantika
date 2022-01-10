333f26d9bd4c7f2c443bb2d3aa9268bf92398490
SATRIO <satriotol69@gmail.com>
2022-01-10 15:32:36 +0700

    payment control try catch

--- app/Http/Controllers/v1/PaymentController.php
+++ app/Http/Controllers/v1/PaymentController.php
@@ -32,32 +32,32 @@ class PaymentController extends Controller
     {
 
         DB::beginTransaction();
-        // try {
-        $orders = Order::with('payment')->where('status', Order::STATUS1)->whereHas('payment', function ($q) {
-            $q->where('payment_type_id', 2);
-        })->where('expired_at', '<', Carbon::now())->get();
-        if (empty($orders)) {
-            return $this->sendFailedResponse([], 'Data Tidak Ditemukan');
+        try {
+            $orders = Order::with('payment')->where('status', Order::STATUS1)->whereHas('payment', function ($q) {
+                $q->where('payment_type_id', 2);
+            })->where('expired_at', '<', Carbon::now())->get();
+            if (empty($orders)) {
+                return $this->sendFailedResponse([], 'Data Tidak Ditemukan');
+            }
+
+            foreach ($orders as $order) {
+                $payment = Payment::where('order_id', $order->id)->first();
+                $order->update([
+                    'status' => Order::STATUS2,
+                    'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
+
+                ]);
+                $payment->update([
+                    'status' => Payment::STATUS2,
+                    'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
+                ]);
+            }
+            DB::commit();
+            return $this->sendSuccessResponse([
+                'order' => $order
+            ], 'Data Pembayaran Berhasil Di Update');
+        } catch (\Throwable $th) {
+            DB::rollback();
         }
-        
-        foreach ($orders as $order) {
-            $payment = Payment::where('order_id', $order->id)->first();
-            $order->update([
-                'status' => Order::STATUS2,
-                'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
-    
-            ]);
-            $payment->update([
-                'status' => Payment::STATUS2,
-                'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
-            ]);
-        }
-        // DB::commit();
-        return $this->sendSuccessResponse([
-            'order' => $order
-        ], 'Data Pembayaran Berhasil Di Update');
-        // } catch (\Throwable $th) {
-        //     DB::rollback();
-        // }
     }
 }
