46b8f0f5fce3afd74c0b5ee40230802640bb674d
SATRIO <satriotol69@gmail.com>
2022-01-10 15:27:35 +0700

    payment controller

--- app/Http/Controllers/v1/PaymentController.php
+++ app/Http/Controllers/v1/PaymentController.php
@@ -32,30 +32,30 @@ class PaymentController extends Controller
     {
 
         DB::beginTransaction();
-        try {
-            $order = Order::with('payment')->where('status', Order::STATUS1)->whereHas('payment', function ($q) {
-                $q->where('payment_type_id', 2);
-            })->where('expired_at', '<', Carbon::now())->get();
-            if (empty($order)) {
-                return $this->sendFailedResponse([], 'Data Tidak Ditemukan');
-            }
-            $payment = Payment::whereIn('order_id', $order->pluck('id'))->get();
-
-            $order->update([
-                'status' => Order::STATUS2,
-                'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
-
-            ]);
-            $payment->update([
-                'status' => Payment::STATUS2,
-                'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
-            ]);
-            DB::commit();
-            return $this->sendSuccessResponse([
-                'order' => $order
-            ], 'Data Pembayaran Berhasil Di Update');
-        } catch (\Throwable $th) {
-            DB::rollback();
+        // try {
+        $order = Order::with('payment')->where('status', Order::STATUS1)->whereHas('payment', function ($q) {
+            $q->where('payment_type_id', 2);
+        })->where('expired_at', '<', Carbon::now())->get();
+        if (empty($order)) {
+            return $this->sendFailedResponse([], 'Data Tidak Ditemukan');
         }
+        $payment = Payment::whereIn('order_id', $order->pluck('id'))->get();
+
+        $order->update([
+            'status' => Order::STATUS2,
+            'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
+
+        ]);
+        $payment->update([
+            'status' => Payment::STATUS2,
+            'cancelation_reason' => 'Waktu Pembayaran Telah Habis'
+        ]);
+        // DB::commit();
+        return $this->sendSuccessResponse([
+            'order' => $order
+        ], 'Data Pembayaran Berhasil Di Update');
+        // } catch (\Throwable $th) {
+        //     DB::rollback();
+        // }
     }
 }
