c2da0a56f8669170256525aea7668e8c3568023e
unknown <ciptaadhinugraha@gmail.com>
2022-05-30 16:28:31 +0700

    fix ca;culate

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -15,16 +15,16 @@ class OrderController extends Controller
     public function calculateDiscount(ApiCalculateDiscountRequest $request) {
         $fleet_route = FleetRoute::find($request->fleet_route_id);
         $setting = Setting::first();
-        $price_food = $fleet_route->fleet_detail?->fleet?->fleetclass?->price_food;
+        $price_food = ($request->is_food) ? $fleet_route->fleet_detail?->fleet?->fleetclass?->price_food * $request->seat_count : 0;
+        $total_travel = $request->is_travel ? $setting->travel * $request->seat_count : 0;
+        $total_member = $request->is_member ? -($setting->member) * $request->seat_count : 0;
 
-        $price_ticket = $request->price_ticket - $price_food;
+        $price_ticket = $request->price_ticket - ($price_food / $request->seat_count) - ($total_travel / $request->seat_count) - ($total_member / $request->seat_count);
 
         $data = [
-            'total_food'=>$request->is_food 
-            ? $price_food * $request->seat_count 
-            : 0,
-            'total_travel'=>$request->is_travel ? $setting->travel * $request->seat_count : 0,
-            'total_member'=>$request->is_member ? -($setting->member) * $request->seat_count : 0
+            'total_food'=> $price_food,
+            'total_travel'=>$total_travel,
+            'total_member'=>$total_member
         ];
         $price_with_food = $request->price_ticket * $request->seat_count;
         
@@ -37,7 +37,7 @@ class OrderController extends Controller
 
         $data = array_merge($data, [
             'price_ticket'  => $price_ticket + $xendit_charge(),
-            'total_price'   => $price_with_food + $data['total_travel'] + $data['total_member'] + $xendit_charge(),
+            'total_price'   => $price_with_food + $data['total_travel'] + $data['total_member'] + $data['total_food'] + $xendit_charge(),
             'xendit_charge' => $xendit_charge()
         ]);
 
