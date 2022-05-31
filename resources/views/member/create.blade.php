5928df75d78a95bcfd5ea8c716fb313a2add7e5f
unknown <ciptaadhinugraha@gmail.com>
2022-05-31 15:50:15 +0700

    fix membership delete agent

--- resources/views/member/create.blade.php
+++ resources/views/member/create.blade.php
@@ -41,18 +41,6 @@ Member
                         @method('PUT')
                         @endisset
                         <div class="form-group">
-                            <label>Agent</label>
-                            <select name="agency_id" class="form-control select2" id="">
-                                <option value="">Pilih Agent</option>
-                                @foreach ($agencies as $agency)
-                                <option value="{{$agency->id}}" @isset($member) @if ($member->agency_id == $agency->id)
-                                    selected
-                                    @endif
-                                    @endisset>{{$agency->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
                             <label>Nama</label><span class="text-danger">*</span>
                             <input type="text" class="form-control" name="name" required
                                 value="{{isset($member)? $member->name : ''}}" id="">
