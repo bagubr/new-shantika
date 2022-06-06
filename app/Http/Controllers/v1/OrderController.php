fd79a7f794663b962e539f778bf32f0d12fd3bd2
unknown <ciptaadhinugraha@gmail.com>
2022-06-06 14:41:33 +0700

    fix price

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApiCalculateDiscountRequest;
+use App\Models\Agency;
 use App\Models\FleetRoute;
 use App\Models\Route;
 use App\Models\Setting;
 use App\Repositories\UserRepository;
+use App\Utils\PriceTiket;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -15,12 +17,15 @@ class OrderController extends Controller
     public function calculateDiscount(ApiCalculateDiscountRequest $request) {
         $user = UserRepository::findByToken($request->bearerToken());
         $fleet_route = FleetRoute::find($request->fleet_route_id);
+        $departure_agency = Agency::find($request->departure_agency_id);
+        $agency_destiny = Agency::find($request->agency_destiny_id);
+        $date = $request->date;
         $setting = Setting::first();
         $price_food = ($request->is_food) ? $fleet_route->fleet_detail?->fleet?->fleetclass?->price_food * $request->seat_count : 0;
         $total_travel = $request->is_travel ? $setting->travel * $request->seat_count : 0;
         $total_member = $request->is_member ? -($setting->member) * $request->seat_count : 0;
 
-        $price_ticket = $request->price_ticket - ($price_food / $request->seat_count) - ($total_travel / $request->seat_count) - ($total_member / $request->seat_count);
+        $price_ticket = PriceTiket::priceTiket($fleet_route, $departure_agency, $agency_destiny, $date);
 
         // $price_food = @$user?->agencies?->agent?->city?->area_id == 2 
         // ? 0
@@ -41,7 +46,6 @@ class OrderController extends Controller
         };
 
         $data = array_merge($data, [
-            'price_ticket'  => $price_ticket + $xendit_charge(),
             'total_price'   => $price_with_food + $data['total_travel'] + $data['total_member'] + $xendit_charge(),
             'xendit_charge' => $xendit_charge()
         ]);
