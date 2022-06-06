2ecde8b850779d18e13b02840e4a8ce55f0867aa
unknown <ciptaadhinugraha@gmail.com>
2022-06-06 11:11:05 +0700

    fix price agency route

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CheckExpiry;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        CheckExpiry::class
     ];
 
     /**
@@ -24,7 +25,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('check:expiry')->everyMinute()->appendOutputTo('scheduler.log');
+    }
+
+    public function scheduleTimezone()
+    {
+        return 'Asia/Jakarta';
     }
 
     /**
