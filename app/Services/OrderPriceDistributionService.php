256aded21a96195e21ecf481afe8bc604d76d4e0
unknown <ciptaadhinugraha@gmail.com>
2022-06-15 11:08:25 +0700

    fix total deposit

--- app/Services/OrderPriceDistributionService.php
+++ app/Services/OrderPriceDistributionService.php
@@ -49,7 +49,7 @@ class OrderPriceDistributionService {
         $total_price['for_agent'] = -1 * (
             $total_price['ticket_only'] * $setting->commision
         );
-        $total_price['for_owner'] = $total_price['ticket_only'] - $total_price['for_agent'];
+        $total_price['for_owner'] = $total_price['ticket_only'] + $total_price['for_agent'];
         $total_price['total_deposit'] = $total_price['for_owner'] + $total_price['for_food'];
         $total_price['for_owner_with_food'] = $total_price['ticket_only'] + $total_price['for_food'];
         $total_price['for_owner_gross'] = $total_price['ticket_price'] + $total_price['for_agent'];
