453994a9fe4af4d02f6e0d8d490d68acc986ea85
unknown <ciptaadhinugraha@gmail.com>
2022-06-29 10:24:04 +0700

    be area id isset

--- resources/views/status_penumpang/index.blade.php
+++ resources/views/status_penumpang/index.blade.php
@@ -34,7 +34,7 @@ Status Penumpang
                                         <select name="area_id" class="form-control">
                                             <option value="">--PILIH AREA--</option>
                                             @foreach ($areas as $area)
-                                            @if ($area_id == $area->id)
+                                            @if (isset($area_id) && $area_id == $area->id)
                                             <option value="{{$area->id}}" selected>{{$area->name}}</option>
                                             @else
                                             <option value="{{$area->id}}">{{$area->name}}</option>
--- resources/views/user_agent/index.blade.php
+++ resources/views/user_agent/index.blade.php
@@ -32,10 +32,10 @@ User Agent
                         <form action="{{route('user_agent.search')}}" method="GET">
                             <div class="form-group">
                                 <label>Cari Area</label>
-                                <select name="area_id" class="form-control" {{($area_id)?'disabled':''}}>
+                                <select name="area_id" class="form-control" {{(isset($area_id))?'disabled':''}}>
                                     <option value="">--PILIH AREA--</option>
                                     @foreach ($areas as $area)
-                                    @if ($area_id == $area->id)
+                                    @if (isset($area_id) && $area_id == $area->id)
                                     <option value="{{$area->id}}" selected>{{$area->name}}</option>
                                     @else
                                     <option value="{{$area->id}}">{{$area->name}}</option>
