6fcdeb91a59f921b9255bc009231c68e93a69a78
unknown <ciptaadhinugraha@gmail.com>
2022-07-12 16:52:54 +0700

    export setoran

--- resources/views/order_price_distribution/index.blade.php
+++ resources/views/order_price_distribution/index.blade.php
@@ -58,6 +58,23 @@ Setoran
                                 </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
+                                        <label>Area</label>
+                                        <select name="area_id" class="form-control">
+                                            <option value="">--PILIH AREA--</option>
+                                            @foreach ($areas as $area)
+                                            @if ($area_id == $area->id)
+                                            <option value="{{$area->id}}" selected>{{$area->name}}
+                                            </option>
+                                            @else
+                                            <option value="{{$area->id}}">{{$area->name}}
+                                            </option>
+                                            @endif
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
                                         <label>Agen</label>
                                         <select name="agency_id" class="form-control select2">
                                             <option value="">--PILIH AGEN--</option>
@@ -69,23 +86,6 @@ Setoran
                                             @else
                                             <option value="{{$agency->id}}">
                                                 {{$agency->city?->name}}/{{$agency->name}}
-                                            </option>
-                                            @endif
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label>Area</label>
-                                        <select name="area_id" class="form-control">
-                                            <option value="">--PILIH AREA--</option>
-                                            @foreach ($areas as $area)
-                                            @if ($area_id == $area->id)
-                                            <option value="{{$area->id}}" selected>{{$area->name}}
-                                            </option>
-                                            @else
-                                            <option value="{{$area->id}}">{{$area->name}}
                                             </option>
                                             @endif
                                             @endforeach
