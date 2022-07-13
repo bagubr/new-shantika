c19aa3d01eb12fbd28c1e352cf360d962bbbc7f6
unknown <ciptaadhinugraha@gmail.com>
2022-07-13 14:15:12 +0700

    add charge detail riwayat agent

--- app/Http/Resources/Order/OrderDetailAgentResource.php
+++ app/Http/Resources/Order/OrderDetailAgentResource.php
@@ -67,6 +67,7 @@ class OrderDetailAgentResource extends JsonResource
             'commision'                 =>$distribution?->for_agent,
             'review'                    =>$this->review,
             'note'                      =>$this->note,
+            'charge'                    =>$this->charge,
         ];
     }
 
