49e19b97d0ffc439269148cff3c92e0baf8881bb
unknown <ciptaadhinugraha@gmail.com>
2022-07-18 10:00:58 +0700

    fix price customer detail setoran2

--- app/Services/OrderPriceDistributionService.php
+++ app/Services/OrderPriceDistributionService.php
@@ -44,11 +44,11 @@ class OrderPriceDistributionService {
         //  ? 0
         //  : $total_price['for_food'];
         $total_price['for_member'] = $total_member * $order->order_detail->where('is_member', 1)->count();
-        $total_price['for_agent'] =   ($total_price['ticket_price'] - $total_price['for_food']) * $setting->commision ;
+        $total_price['for_agent'] =   (($total_price['ticket_price'] + $setting->xendit_charge) - $total_price['for_food']) * $setting->commision ;
         $total_price['for_owner'] = ($total_price['ticket_only'] - $total_price['for_food']) - $total_price['for_agent'];
         $total_price['total_deposit'] = $total_price['for_owner'] + $total_price['for_food'];
         if($order->agency->city->area_id == 2){
-            $total_price['for_agent'] =   $total_price['ticket_price'] * $setting->commision ;
+            $total_price['for_agent'] =   ($total_price['ticket_price'] + $setting->xendit_charge) * $setting->commision ;
             $total_price['for_owner'] = $total_price['ticket_only'] - $total_price['for_agent'];
             $total_price['total_deposit'] = $total_price['for_owner'];
         }
