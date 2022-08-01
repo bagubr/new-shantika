0b00d1763fca8dca5f391b5b111a48ebf4cc51d4
unknown <ciptaadhinugraha@gmail.com>
2022-08-01 11:38:26 +0700

    fix search fleet class

--- app/Http/Controllers/v1/FleetClassController.php
+++ app/Http/Controllers/v1/FleetClassController.php
@@ -27,29 +27,30 @@ class FleetClassController extends Controller
         $agency_id = $request->agency_id;
         $agency = Agency::find($agency_id);
         $fleet_class = FleetClass::has('fleets.fleet_detail.fleet_route.prices')->select('id', 'name', 'price_food')
+        ->whereHas('prices', function ($query) use ($agency)
+        {
+            $query->where('area_id', $agency->city->area_id);
+        })
         ->whereHas('fleets.fleet_detail', function ($query) use ($time_classification_id, $date, $agency_id, $agency)
         {
-            $query->whereHas('fleet_route', function ($query) use ($date, $agency_id, $agency, $time_classification_id)
+            $query->whereHas('fleet_route', function ($query) use ($date, $agency_id, $time_classification_id)
             {
                 $query->where('is_active', true);
                 $query->whereHas('prices', function ($query) use ($date)
                 {
-                    $query->whereDate('start_at', '<=', $date)->whereDate('end_at', '>=', $date);
+                    $query->whereDate('start_at', '<=', date('Y-m-d', strtotime($date)))->whereDate('end_at', '>=', date('Y-m-d', strtotime($date)));
                 });
-                $query->whereHas('route.checkpoints', function ($query) use ($agency_id, $agency, $time_classification_id)
+                $query->whereHas('route.checkpoints', function ($query) use ($agency_id, $time_classification_id)
                 {
                     $time_start = TimeClassification::find($time_classification_id)->time_start;
                     $time_end = TimeClassification::find($time_classification_id)->time_end;
                     $query->whereHas('agency.agent_departure', function($subquery) use ($time_start, $time_end) {
                         $subquery->where('departure_at', '>', $time_start)->orWhere('departure_at', '<', $time_end);
                     });
-                    $query->whereHas('agency', function ($query) use ($agency)
+                    $query->where('agency_id', $agency_id);
+                    $query->whereHas('agency', function ($query)
                     {
                         $query->where('is_active', true);
-                        $query->whereHas('city.area', function ($query) use ($agency)
-                        {
-                            $query->where('id', $agency->city->area_id);
-                        });
                     });
                 });
                 $query->where(function ($query) use ($agency_id, $date)
@@ -70,6 +71,7 @@ class FleetClassController extends Controller
             });
         })
         ->get()->makeHidden(['price_fleet_class1', 'price_fleet_class2']);
+        
 
         return $this->sendSuccessResponse([
             'fleet_classes'=>$fleet_class
