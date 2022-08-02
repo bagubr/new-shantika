73a1a0004c86dcbf6b85a786b9b55796321b785b
unknown <ciptaadhinugraha@gmail.com>
2022-08-02 10:48:04 +0700

    fix search class

--- app/Http/Controllers/v1/FleetClassController.php
+++ app/Http/Controllers/v1/FleetClassController.php
@@ -8,6 +8,7 @@ use App\Models\Agency;
 use App\Models\FleetClass;
 use App\Models\TimeClassification;
 use App\Repositories\AgencyRepository;
+use App\Repositories\FleetRouteRepositories;
 use App\Repositories\TimeClassificationRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
@@ -26,20 +27,17 @@ class FleetClassController extends Controller
         $date = $request->date;
         $agency_id = $request->agency_id;
         $agency = Agency::find($agency_id);
+        $area_id = $agency->city->area_id;
         $fleet_class = FleetClass::has('fleets.fleet_detail.fleet_route.prices')->select('id', 'name', 'price_food')
-        ->whereHas('prices', function ($query) use ($agency)
+        ->whereHas('fleets.fleet_detail.fleet_route.prices', function ($query) use ($date)
         {
-            $query->where('area_id', $agency->city->area_id);
+            $query->whereDate('start_at', '<=', $date)->whereDate('end_at', '>=', $date);
         })
-        ->whereHas('fleets.fleet_detail', function ($query) use ($time_classification_id, $date, $agency_id, $agency)
+        ->whereHas('fleets.fleet_detail', function ($query) use ($time_classification_id, $date, $agency_id, $area_id)
         {
-            $query->whereHas('fleet_route', function ($query) use ($date, $agency_id, $time_classification_id)
+            $query->whereHas('fleet_route', function ($query) use ($date, $agency_id, $time_classification_id, $area_id)
             {
                 $query->where('is_active', true);
-                $query->whereHas('prices', function ($query) use ($date)
-                {
-                    $query->whereDate('start_at', '<=', date('Y-m-d', strtotime($date)))->whereDate('end_at', '>=', date('Y-m-d', strtotime($date)));
-                });
                 $query->whereHas('route.checkpoints', function ($query) use ($agency_id, $time_classification_id)
                 {
                     $time_start = TimeClassification::find($time_classification_id)->time_start;
@@ -53,11 +51,19 @@ class FleetClassController extends Controller
                         $query->where('is_active', true);
                     });
                 });
-                $query->where(function ($query) use ($agency_id, $date)
+                $query->where(function ($query) use ($agency_id, $date, $area_id)
                 {
-                    $query->whereHas('route.checkpoints', function ($query) use ($agency_id)
+                    $query->whereHas('route.checkpoints', function ($query) use ($agency_id, $area_id)
                     {
                         $query->where('agency_id', $agency_id);
+                        $query->whereHas('agency', function ($query) use ($area_id)
+                        {
+                            $query->where('is_active', true);
+                            $query->whereHas('city.area', function ($query) use ($area_id)
+                            {
+                                $query->where('id', $area_id);
+                            });
+                        });
                     });
                     $query->orWhere(function ($query) use ($agency_id, $date)
                     {
@@ -71,10 +77,18 @@ class FleetClassController extends Controller
             });
         })
         ->get()->makeHidden(['price_fleet_class1', 'price_fleet_class2']);
-        
-
+        foreach($fleet_class as $key => $value){
+            $user = UserRepository::findByToken($request->bearerToken());
+            $agency_id = $request->agency_departure_id??$user->agencies->agency_id;
+            $departure_agency = AgencyRepository::findWithCity($agency_id);
+            $data = FleetRouteRepositories::search_fleet($date, $departure_agency, $agency, $time_classification_id, $value->id);
+            if(count($data) > 0){
+               $fleet_classses[] = $value;
+            }
+        }
         return $this->sendSuccessResponse([
-            'fleet_classes'=>$fleet_class
+            'fleet_classes'=>$fleet_classses,
+            // 'data' => $data
         ]);
     }
 }
